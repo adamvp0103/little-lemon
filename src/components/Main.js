0c68f834c0fdc9255bcec647323c1bfbe0297a9d
Adam Parks <adamvp0103@gmail.com>
2025-08-19 12:53:22 -0400

    Set up semantic sections

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <main>
+      <img
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmeta-capstone-little-lemon.web.app%2F&psig=AOvVaw3s7jBpPc-okbeo68sW9pdk&ust=1755708468373000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCIjhnfWpl48DFQAAAAAdAAAAABAV"
+        alt="Little Lemon logo"
+      />
+    </main>
+  );
+}
+
+export default Main;
