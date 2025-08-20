41cd483f759899f2fd88e24ef8b49ead8a139864
Adam Parks <adamvp0103@gmail.com>
2025-08-20 14:14:40 -0400

    Add meta tags and OGP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <img
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmeta-capstone-little-lemon.web.app%2F&psig=AOvVaw3s7jBpPc-okbeo68sW9pdk&ust=1755708468373000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCIjhnfWpl48DFQAAAAAdAAAAABAV"
+        alt="Little Lemon logo"
+      />
+    </header>
+  );
 }
 
 export default Header;
